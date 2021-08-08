de197575f3e6fda98fb57141fc5559f67d20237a
Ammar Ali <m.ammarali98@gmail.com>
2021-08-09 03:08:06 +0500

    Added angular material and some styling

--- mean-stack/src/app/app.module.ts
+++ mean-stack/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppComponent } from './app.component';
 import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
@@ -12,7 +16,11 @@ import { PostsCreateComponent } from './posts/posts-create/posts-create.componen
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
