7ca657c0bc38cf55a1b02587d13eff5a495c6104
Ammar Ali <m.ammarali98@gmail.com>
2021-08-09 02:02:26 +0500

    Added post-create component

--- mean-stack/src/app/app.module.ts
+++ mean-stack/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ mean-stack/src/app/posts/posts-create/posts-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-create',
+  templateUrl: './posts-create.component.html',
+  styleUrls: ['./posts-create.component.css']
+})
+export class PostsCreateComponent {
+
+  constructor() { }
+
+}
