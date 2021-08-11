be6b6627058c03374539342f834b486b91c3b080
Ammar Ali <m.ammarali98@gmail.com>
2021-08-11 11:34:59 +0500

    Added accordions and posts to post list

--- mean-stack/src/app/posts/post-list/post-list.component.ts
+++ mean-stack/src/app/posts/post-list/post-list.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
+  posts = [
+    { title: "My First Post", content: "This is my first post here!" },
+    { title: "My Second Post", content: "This is my second post here!" },
+    { title: "My Third Post", content: "This is my third post here!" },
+  ]
+
+  // posts = []
 
   constructor() { }
 
